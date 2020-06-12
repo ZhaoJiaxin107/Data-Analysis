411a2b52191ef1ee7627ff60d3769897a4541290
Jiaxin Zhao <jzhao537@uwo.ca>
2020-06-11 20:19:51 -0400

    commit index page predict

--- app.py
+++ app.py
@@ -9,4 +9,20 @@ CORS(app)
 def default():
     return '<h1> API Server is working </h1>'
 
-app.run()
+@app.route('/predict')
+def predict():
+    from sklearn.externals import joblib
+    model = joblib.load('marriage_age_predict_model.ml')
+    predicted_age_of_marriage = model.predict([[int(request.args['gender']),
+                                  int(request.args['religion']),
+                                  int(request.args['caste']),
+                                  int(request.args['mother_tongue']),
+                                  int(request.args['country']),
+                                  int(request.args['height_cms'])
+    
+    
+    ]])
+    return str(round(predicted_age_of_marriage[0],2))
+
+if __name__ == "__main__":
+    app.run(debug=True)
